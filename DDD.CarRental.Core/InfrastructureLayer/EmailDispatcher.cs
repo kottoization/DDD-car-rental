ab81a8d7e0965834c102ae3cda001b6acfb5cad6
kottoization <96475096+kottoization@users.noreply.github.com>
2023-05-31 15:13:14 +0200

    email interface

---
+++ DDD.CarRental.Core/InfrastructureLayer/EmailDispatcher.cs
@@ -0,0 +1,18 @@
+﻿using DDD.CarRental.Core.DomainModelLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.CarRental.Core.InfrastructureLayer
+{
+    internal class EmailDispatcher : IEmailDispatcher
+    {
+        public void Send(MailMessage mailMessage)
+        {
+            // put email sending code here ...
+        }
+    }
+}
